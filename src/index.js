87225b8af460c6d2c5554f1a4e6058fcab546fc8
omar7altawil <omar.altawil@hotmail.com>
2019-06-14 18:44:36 +0200

    move Firebase Initializing to a config file

--- src/index.js
+++ src/index.js
@@ -3,20 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase'
-
-var firebaseConfig = {
-  apiKey: "AIzaSyCpc6o1X4c_vIxMafO2QE29aBDM4w-UPio",
-  authDomain: "tum-market.firebaseapp.com",
-  databaseURL: "https://tum-market.firebaseio.com",
-  projectId: "tum-market",
-  storageBucket: "tum-market.appspot.com",
-  messagingSenderId: "1018647233485",
-  appId: "1:1018647233485:web:ee411c8fc9da26f3"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
