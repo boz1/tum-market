9f7e6bada20fc9a0b35fadcc8d04da14165a4941
Burak Oz <oz.burak1995@gmail.com>
2019-06-21 15:02:32 +0200

    New trade request implemented

--- src/components/advertisement/Advertisement.js
+++ src/components/advertisement/Advertisement.js
@@ -10,10 +10,11 @@ export default class Advertisement extends Component {
         }
     }
 
+
     render() {
         return (
             <Card style={{ width: '18rem', height: 'fit-content' }}>
-                <Link to={{ pathname: '/adDetails/' + this.props.ad.id, state: { ad: this.props.ad, user: this.props.user} }}><Image
+                <Link to={{ pathname: '/adDetails/' + this.props.ad.id, state: { ad: this.props.ad, user: this.props.user } }}><Image
                     src={this.props.ad.image}
                     height={240}
                 /></Link>
@@ -25,7 +26,7 @@ export default class Advertisement extends Component {
                         {this.props.ad.mainCategory}
                     </Card.Text>
                     <Card.Text className="bold">
-                    <span className="text-premium">{this.props.ad.price + " €"}</span>  {(this.props.ad.trade ? <span className="text-trade">Trade</span> : "")}
+                        <span className="text-premium">{this.props.ad.price + " €"}</span>  {(this.props.ad.trade ? <span className="text-trade">Trade</span> : "")}
                     </Card.Text>
                 </Card.Body>
             </Card>
