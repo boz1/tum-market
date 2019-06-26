8a167d08c08641c610073ee2f09354978a9d5c7c
omar7altawil <omar.altawil@hotmail.com>
2019-06-27 00:59:25 +0200

    fix receive props

--- src/components/advertisement/AdvertisementList.js
+++ src/components/advertisement/AdvertisementList.js
@@ -12,12 +12,15 @@ export default class AdvertisementList extends Component {
             user:this.props.user
         }
         this.sort=this.sort.bind(this)
+        this.componentWillReceiveProps=this.componentWillReceiveProps.bind(this)
     }
-sort(){
+    componentWillReceiveProps(){
+        this.setState({data:this.props.adsList})
+    }
+    sort(){
     
-}
+    }
     render() {
-        let data = this.props.adsList;
         return (
             <React.Fragment>
                 <div className='col-md-12 d-flex'>
@@ -33,13 +36,13 @@ sort(){
                         <span  style={{paddingLeft:"80%"}} onClick={this.sort}>
                             <i class="fa fa-sort-alpha-desc" aria-hidden="true"></i>
                             </span>
-                            <span  style={{paddingLeft:"75%"}} onClick={data.reverse}>
+                            <span  style={{paddingLeft:"75%"}} onClick={this.state.data.reverse}>
                             <i class="fa fa-sort-alpha-asc" aria-hidden="true"></i>
                         </span>
                         </form>
                         <hr className="my-2"></hr>
                         <CardDeck className="mb-5">
-                            {data.length > 0 ? data.map((ad) => <Advertisment key={ad.id} ad={ad} user={this.state.user}/>) : ""}
+                            {this.state.data.length > 0 ? this.state.data.map((ad) => <Advertisment key={ad.id} ad={ad} user={this.state.user}/>) : ""}
                         </CardDeck>
                     </div>
                 </div>
