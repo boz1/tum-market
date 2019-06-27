e053a77eb06c4d79125600472c465a413a268307
omar7altawil <omar.altawil@hotmail.com>
2019-06-27 14:08:27 +0200

    redirect Verification

--- src/components/Verification.js
+++ src/components/Verification.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import firebase from '../config/firebaseConfig';
 
-export default class PageNotFound extends Component {
+export default class Verification extends Component {
     render() {
+        var user = firebase.auth().currentUser;
+        console.log(user)
         return (
             <div>
                 <h1>Please verfy your email address</h1>
