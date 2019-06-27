3c00a14fe621367fe0c168397fd571f7d0286391
Burak Oz <oz.burak1995@gmail.com>
2019-06-28 00:40:36 +0200

    Validation for new ad

--- src/components/advertisement/NewAdvertisement.js
+++ src/components/advertisement/NewAdvertisement.js
@@ -15,8 +15,9 @@ export default class NewAdvertisement extends Component {
             price: 0,
             description: '',
             image: '',
-            trade: false,
-            showSub: false
+            trade: "On",
+            showSub: false,
+            validated: false
         }
 
         this.handleMainCatChange = this.handleMainCatChange.bind(this)
@@ -25,7 +26,9 @@ export default class NewAdvertisement extends Component {
         this.handleTitleChange = this.handleTitleChange.bind(this)
         this.handleConditionChange = this.handleConditionChange.bind(this)
         this.handlePriceChange = this.handlePriceChange.bind(this)
-        this.showResults = this.showResults.bind(this)
+        this.handleTradeChange = this.handleTradeChange.bind(this)
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
 
@@ -65,6 +68,20 @@ export default class NewAdvertisement extends Component {
         });
     }
 
+    handleTradeChange(event) {
+        const trade = event.target.value
+        this.setState({
+            trade: trade
+        });
+    }
+
+    handleDescriptionChange(event) {
+        const description = event.target.value
+        this.setState({
+            description: description
+        });
+    }
+
     getMainCatId() {
         let id;
         this.props.categories.forEach(element => {
@@ -75,8 +92,14 @@ export default class NewAdvertisement extends Component {
         return id;
     }
 
-    showResults() {
-        console.log(this.state)
+    handleSubmit(event) {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        console.log(form)
+        this.setState({ validated: true });
     }
 
 
@@ -88,29 +111,34 @@ export default class NewAdvertisement extends Component {
         let mainCatContainer, subCatContainer, conditionsContainer;
 
         if (this.props.categories.length > 0) {
-            mainCategories.push(<option key="-1" disabled>Choose...</option>)
+            mainCategories.push(<option key="empty" disabled value={''}>Choose...</option>)
             this.props.categories.map((cat) => mainCategories.push(<option key={cat.id + cat.title}>{cat.title}</option>))
-            mainCatContainer = <Form.Control as="select" defaultValue="Choose..." onChange={this.handleMainCatChange}>
+            mainCatContainer = <Form.Control required as="select" defaultValue={''} onChange={this.handleMainCatChange}>
                 {mainCategories}
             </Form.Control>
         }
 
         if (this.props.subCategories.length > 0 && this.state.showSub) {
             let id = this.getMainCatId();
-            subCategories.push(<option key="-1" disabled>Choose...</option>)
+            subCategories.push(<option key="empty" disabled value={''}>Choose...</option>)
             this.props.subCategories[id].map((cat) => subCategories.push(<option key={cat.id + cat.title}>{cat.title}</option>))
-            subCatContainer = <Form.Control as="select" defaultValue="Choose..." onChange={this.handleSubCatChange}>
+            subCatContainer = <Form.Control required as="select" defaultValue={''} onChange={this.handleSubCatChange}>
                 {subCategories}
             </Form.Control>
         }
 
         if (this.props.conditions.length > 0) {
-            conditions.push(<option key="-1" disabled>Choose...</option>)
+            conditions.push(<option key="empty" disabled value={''}>Choose...</option>)
             this.props.conditions.map((con) => conditions.push(<option key={con.id + con.title}>{con.title}</option>))
-            conditionsContainer = <Form.Control as="select" defaultValue="Choose..." onChange={this.handleConditionChange}>
+            conditionsContainer = <Form.Control required as="select" defaultValue={''} onChange={this.handleConditionChange}>
                 {conditions}
             </Form.Control>
         }
+
+        const okFeedback = <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        const invalidDropdownFeedback = <Form.Control.Feedback type="invalid">Please choose a value!</Form.Control.Feedback>
+        const invalidTextFeedback = <Form.Control.Feedback type="invalid">Please enter a value!</Form.Control.Feedback>
+        const { validated } = this.state;
 
         return (
             <React.Fragment>
@@ -121,50 +149,69 @@ export default class NewAdvertisement extends Component {
                     <div className="col-md-9">
                         <Title title="Create New Advertisement" />
                         <hr className="my-2"></hr>
-                        <Form>
+                        <Form
+                            validated={validated}
+                            onSubmit={e => this.handleSubmit(e)}>
                             <div className="col-md-12 d-flex p-0">
                                 <div className="col-md-9 pl-0">
                                     <div className="d-flex col-sm-12 p-0">
                                         <div className="col-sm-6 pl-0">
-                                            <Form.Group controlId="exampleForm.ControlSelect1">
+                                            <Form.Group>
                                                 <Form.Label className="text-sub-title" style={{ fontSize: "16px" }} >Main Category</Form.Label>
                                                 {mainCatContainer}
+                                                {this.state.mainCategory !== '' ? okFeedback : invalidDropdownFeedback}
                                             </Form.Group>
-                                            <Form.Group controlId="exampleForm.ControlSelect2" >
+                                            <Form.Group >
                                                 <Form.Label className="text-sub-title" style={{ fontSize: "16px" }}>Sub Category</Form.Label>
                                                 {subCatContainer}
+                                                {this.state.subCategory !== '' ? okFeedback : invalidDropdownFeedback}
                                             </Form.Group>
-                                            <Form.Group controlId="exampleForm.ControlSelect3">
+                                            <Form.Group>
                                                 <Form.Label className="text-sub-title" style={{ fontSize: "16px" }}>Condition</Form.Label>
                                                 {conditionsContainer}
+                                                {this.state.condition !== '' ? okFeedback : invalidDropdownFeedback}
                                             </Form.Group>
                                         </div>
                                         <div className="col-sm-6 ml-2">
-                                            <Form.Group controlId="formHorizontalEmail">
+                                            <Form.Group>
                                                 <Form.Label className="text-sub-title pl-0" style={{ fontSize: "16px" }}>
                                                     Title
                                                  </Form.Label>
-                                                <Form.Control type="text" placeholder="Title" value={this.state.title} onChange={this.handleTitleChange} />
+                                                <Form.Control required type="text" placeholder="Title" onChange={this.handleTitleChange} pattern="[a-zA-Z0-9]{5,40}" title="Title can't be less than 5 and more than 40 characters, and can only contain alphanumeric characters." />
                                             </Form.Group>
-
-                                            <Form.Group controlId="exampleForm.ControlSelect3">
-                                                <Form.Label className="text-sub-title mr-3" style={{ fontSize: "16px" }}>Trade</Form.Label>
-                                                <Form.Check inline label="On" type="radio" id='inline-radio-1' className="mr-2" />
-                                                <Form.Check inline label="Off" type="radio" id='inline-radio-2' className="m-auto" />
-                                            </Form.Group>
-                                            <Form.Group controlId="formHorizontalEmail">
+                                            <div>
+                                                <span className="text-sub-title pl-0" style={{ fontSize: "16px" }} >Trade</span>
+                                                <label className="ml-4">
+                                                    <input
+                                                        type="radio"
+                                                        value="On"
+                                                        checked={this.state.trade === "On"}
+                                                        onChange={this.handleTradeChange}
+                                                    />
+                                                    <span style={{ marginLeft: "5px" }}>On</span>
+                                                </label>
+                                                <label className="ml-2">
+                                                    <input
+                                                        type="radio"
+                                                        value="Off"
+                                                        checked={this.state.trade === "Off"}
+                                                        onChange={this.handleTradeChange}
+                                                    />
+                                                    <span style={{ marginLeft: "5px" }}>Off</span>
+                                                </label>
+                                            </div>
+                                            <Form.Group>
                                                 <Form.Label className="text-sub-title pl-0" style={{ fontSize: "16px" }}>
-                                                    Price
+                                                    Price (€)
                                                  </Form.Label>
-                                                <Form.Control type="number" placeholder="€" value={this.state.price} onChange={this.handlePriceChange} />
+                                                <Form.Control required type="number" placeholder="€" onChange={this.handlePriceChange} min={0} />
                                             </Form.Group>
-
                                         </div>
                                     </div>
                                     <div className="my-1">
-                                        <Form.Group controlId="exampleForm.ControlTextarea1" className="pl-0">
-                                            <Form.Label className="text-sub-title" style={{ fontSize: "16px" }}>Description</Form.Label>
-                                            <Form.Control as="textarea" rows="3" />
+                                        <Form.Group className="pl-0">
+                                            <Form.Label className="text-sub-title" style={{ fontSize: "16px" }}>Description <span style={{color:"#707070", fontSize:"14px"}}>(max 250 characters)</span></Form.Label>
+                                            <Form.Control required as="textarea" rows="3" onChange={this.handleDescriptionChange} maxLength="250" placeholder="Descibe your product..."/>
                                         </Form.Group>
                                     </div>
                                 </div>
@@ -173,7 +220,7 @@ export default class NewAdvertisement extends Component {
                                         <span className="text-sub-title" style={{ fontSize: "16px" }}>Preview</span>
                                     </div>
                                     <div>
-                                        <Button variant="primary" onClick={this.showResults}>
+                                        <Button variant="primary" type="submit">
                                             Complete
                                         </Button>
                                     </div>
