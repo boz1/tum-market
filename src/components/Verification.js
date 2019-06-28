11ce7b7d9528d70910bab7d9c25151fc682444ba
omar7altawil <omar.altawil@hotmail.com>
2019-06-28 15:43:10 +0200

    Verification route

--- src/components/Verification.js
+++ src/components/Verification.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react'
 import firebase from '../config/firebaseConfig';
 
 export default class Verification extends Component {
-    render() {
-        var user = firebase.auth().currentUser;
+    constructor(props) {
+        super(props);
+        this.state={
+            text:null
+        }
+        this.verfy = this.verfy.bind(this);
+      }
+    
+
+    verfy(){
+        const user = firebase.auth().currentUser;
         console.log(user)
+        user.sendEmailVerification().then(function() {
+        }).catch(function(error) {
+            console.log(error)
+        });
+        this.setState({text:"email sent to"+user.email})
+    }
+    render() {
         return (
             <div>
                 <h1>Please verfy your email address</h1>
-                <button>Send verification again</button>
+                <button onClick={this.verfy}>Send verification</button>
+                <div>{this.state.text ? <h3> this.state.text</h3> : <div></div>}</div>
             </div>
+            
         )
     }
 }
