587b7ed8224c6b9151b36c85da28748a4b58bd9c
Burak Oz <oz.burak1995@gmail.com>
2019-06-28 20:29:07 +0200

    Sign Up and Verification frontend and validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import firebase from '../config/firebaseConfig';
 import Alert from 'react-bootstrap/Alert'
 import history from '../history'
-import { Link } from 'react-router-dom'
 import { Switch, Route } from 'react-router-dom'
 import Verification from './Verification'
 
@@ -14,18 +13,18 @@ class Login extends Component {
       email: '',
       password: '',
       showAlert: false,
-      signupBool:false,
-      name:'',
-      address:'',
-      mobile:'',
-      error:{}
+      signupBool: false,
+      name: '',
+      address: '',
+      mobile: ''
+      // error: {}
     };
 
     this.signup = this.signup.bind(this);
     this.togglePage = this.togglePage.bind(this);
     this.login = this.login.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.validateForm = this.validateForm.bind(this);
+    // this.validateForm = this.validateForm.bind(this);
   }
 
   componentDidMount() {
@@ -39,41 +38,41 @@ class Login extends Component {
   login(e) {
     e.preventDefault();
     firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-    .then((u) => {console.log(u)})
-    .catch((error) => {
-      console.log(error)
-      this.setState({
-        showAlert: true,
-        password: ''
-      })
-    });
+      .then((u) => { console.log(u) })
+      .catch((error) => {
+        console.log(error)
+        this.setState({
+          showAlert: true,
+          password: ''
+        })
+      });
   }
 
   togglePage() {
-    this.setState({email:"",password:""})
+    this.setState({ email: "", password: "" })
     this.setState({ signupBool: !this.state.signupBool })
   }
 
-  validateForm() {
+  /*validateForm() {
 
     let errors = {};
     let formIsValid = true;
 
     if (!this.state.name) {
       formIsValid = false;
-      errors["name"] = "*Please enter your username.";
+      errors["name"] = "*Please enter your full name.";
     }
 
     if (typeof this.state.name !== "undefined") {
       if (!this.state.name.match(/^[a-zA-Z ]*$/)) {
         formIsValid = false;
-        errors["name"] = "*Please enter alphabet characters only.";
+        errors["name"] = "*Please enter alphabetic characters only.";
       }
     }
 
     if (!this.state.email) {
       formIsValid = false;
-      errors["email"] = "*Please enter your email-ID.";
+      errors["email"] = "*Please enter your TUM email.";
     }
 
     if (typeof this.state.email !== "undefined") {
@@ -81,19 +80,19 @@ class Login extends Component {
       var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))/i);
       if (!pattern.test(this.state.email)) {
         formIsValid = false;
-        errors["email"] = "*Please enter valid email-ID.";
+        errors["email"] = "*Please enter a valid TUM email.";
       }
     }
 
     if (!this.state.mobile) {
       formIsValid = false;
-      errors["mobile"] = "*Please enter your mobile no.";
+      errors["mobile"] = "*Please enter your mobile number.";
     }
 
     if (typeof this.state.mobile !== "undefined") {
       if (!this.state.mobile.match(/^[0-9]{10}$/)) {
         formIsValid = false;
-        errors["mobile"] = "*Please enter valid Number.";
+        errors["mobile"] = "*Please enter valid number.";
       }
     }
 
@@ -105,7 +104,7 @@ class Login extends Component {
     if (typeof this.state.password !== "undefined") {
       if (!this.state.password.match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
         formIsValid = false;
-        errors["password"] = "*Please enter secure and strong password.";
+        errors["password"] = "*Please enter a secure password with at least 8 characters.";
       }
     }
     if (!this.state.address) {
@@ -117,20 +116,22 @@ class Login extends Component {
     });
     console.log(errors)
     return formIsValid;
-  }
+  }*/
+
   signup(e) {
-    if(this.validateForm()){
+    // if (this.validateForm()) {
     e.preventDefault();
-    firebase.auth().createUserWithEmailAndPassword(this.state.email+"@mytum.du", this.state.password).then((u) => {
-    }).then((u) => { console.log(u) })
+    firebase.auth().createUserWithEmailAndPassword(this.state.email + "@mytum.du", this.state.password).then((u) => {
+    }).then((u) => {
+      history.push('/verify')
+    })
       .catch((error) => {
         console.log(error);
       })
-    }
+    // }
   }
 
   render() {
-
     if (!this.state.signupBool) {
       return (
         <div className="container mt-5">
@@ -138,12 +139,12 @@ class Login extends Component {
             <h3 className="login-header">Student Login</h3>
             <hr></hr>
             <form>
-              <div className="form-group large-text" >
-                <label htmlFor="exampleInputEmail1">Email</label>
+              <div className="form-group text-sub-title" >
+                <label >Email</label>
                 <input value={this.state.email} onChange={this.handleChange} type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email address" />
                 {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
               </div>
-              <div className="form-group large-text">
+              <div className="form-group text-sub-title">
                 <label htmlFor="exampleInputPassword1">Password</label>
                 <input value={this.state.password} onChange={this.handleChange} type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Enter password" />
               </div>
@@ -155,7 +156,7 @@ class Login extends Component {
                   <button type="submit" onClick={this.login} className="btn btn-primary">Login</button>
                 </div>
                 <div className="ml-auto mt-auto mb-auto  large-text">
-                  <span>New here? <strong style={{cursor:"pointer"}} onClick={this.togglePage} className="text-premium bold">Sign Up!</strong></span>
+                  <span>New here? <strong style={{ cursor: "pointer" }} onClick={this.togglePage} className="text-premium bold">Sign Up!</strong></span>
                 </div>
               </div>
             </form>
@@ -166,61 +167,60 @@ class Login extends Component {
     else {
       return (
         <Switch>
-        <Route path="/verify" render={(props) => <Verification/>} />
-        <div className="container mt-5">
-          <div className="mx-auto my-auto p-4 w-50 login-border">
-            <h3 className="login-header">Create New Account</h3>
-            <hr></hr>
-            <label htmlFor="exampleInputEmail1" className="large-text">Email</label>
-            <form className="form-inline">
-              <div className="form-group mb-2">
-                <input name="email"  onChange={this.handleChange}value={this.state.email} type="text" className="form-control" placeholder="TUM Email" />
+          <Route path="/verify" component={Verification} />
+          <React.Fragment>
+            <div className="container mb-3 mt-3">
+              <div className="mx-auto my-auto p-4 w-50 login-border">
+                <h3 className="login-header">Create New Account</h3>
+                <hr></hr>
+                <label className="text-sub-title">Email</label>
+                <form onSubmit={e => this.signup(e)}>
+                  <div className="d-flex">
+                    <div className="form-group mb-2">
+                      <input required name="email" onChange={this.handleChange} type="text" className="form-control  w-100" placeholder="TUM Email" />
+                    </div>
+                    <div className="form-group mx-sm-3 mb-2">
+                      <div className="input-group-prepend">
+                        <div className="input-group-text">@mytum.du</div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <div style={{ color: "red" }}>{this.state.error.email}</div> */}
+                  <label className="text-sub-title">Password</label>
+                  <div className="form-group mb-2">
+                    <input required name="password" onChange={this.handleChange} type="password" className="form-control w-75" placeholder="Password" minLength="8" maxLength="20" />
+                  </div>
+                  {/* <div style={{ color: "red" }}>{this.state.error.password}</div> */}
+                  <label className="text-sub-title">Full Name</label>
+                  <div className="form-group mb-2">
+                    <input required pattern="[a-zA-Z\s]{1,40}" title="Name can only have alphabetic characters. (max 40 char.)" maxLength="40" name="name" onChange={this.handleChange} type="text" className="form-control w-75" placeholder="Name + Last Name" />
+                  </div>
+                  {/* <div style={{ color: "red" }}>{this.state.error.name}</div> */}
+                  <label className="text-sub-title">Mobile Number</label>
+                  <div className="form-group mb-2">
+                    <input pattern="[0-9+\s]{10,20}" title="Mobile number can only have +, space, and numeric characters. (min 10, max 20 char.)" required name="mobile" onChange={this.handleChange} type="tel" className="form-control w-75" placeholder="Mobile" maxLength="20" />
+                  </div>
+                  {/* <div style={{ color: "red" }}>{this.state.error.mobile}</div> */}
+                  <label className="text-sub-title">Address <span style={{ color: "#707070", fontSize: "14px" }}>(max 100 characters)</span></label>
+                  <div className="form-group mb-2">
+                    <input required name="address" onChange={this.handleChange} type="text" className="form-control" placeholder="Address" maxLength="100" />
+                  </div>
+                  {/* <div style={{ color: "red" }}>{this.state.error.address}</div> */}
+                  <div className="d-flex mt-3">
+                    <div>
+                      <button type="submit" className="btn btn-primary">
+                        Sign Up
+                    </button>
+                    </div>
+                    <div className="ml-auto mt-auto mb-auto  large-login valdationtext">
+                      <span>Already have an account? <strong style={{ cursor: "pointer" }} onClick={this.togglePage} className="text-premium bold">Login!</strong></span>
+                    </div>
+                  </div>
+                </form>
               </div>
-              <div className="form-group mx-sm-3 mb-2">
-                <div className="input-group-prepend">
-                  <div className="input-group-text">@mytum.du</div>
-                </div>
-              </div>
-            </form>
-            <div style={{  color: "red"}}>{this.state.error.email}</div>
-            <label htmlFor="exampleInputEmail1" className="large-text">Password</label>
-            <div className="form-group mb-2">
-              <input name="password" onChange={this.handleChange} value={this.state.password} type="password" className="form-control" placeholder="Password" />
             </div>
-            <div style={{  color: "red"}}>{this.state.error.password}</div>
-            <label htmlFor="exampleInputEmail1" className="large-text">Details</label>
-            <form className="form-inline">
-            <div className="form-group mx-sm-3 mb-2">
-              <input name="name" onChange={this.handleChange} value={this.state.name} type="text" className="form-control" placeholder="Name" />
-            </div>
-            <div style={{  color: "red"}}>{this.state.error.name}</div>
-            </form>
-            <form className="form-inline">
-              <div className="form-group mx-sm-3 mb-2">
-                <input name="address" onChange={this.handleChange} value={this.state.address} type="text" className="form-control" placeholder="Address" />
-              </div>
-              <div style={{  color: "red"}}>{this.state.error.address}</div>
-              </form>
-              <form className="form-inline">
-              <div className="form-group mx-sm-3 mb-2">
-                <input name="mobile" onChange={this.handleChange} value={this.state.mobile} type="text"  className="form-control" placeholder="Mobile" />
-              </div>
-              <div style={{  color: "red"}}>{this.state.error.mobile}</div>
-              </form>
-            <div className="d-flex mt-2">
-              <div>
-                <button type="submit" onClick={this.signup} className="btn btn-primary">  
-                 {!this.state.error ? <Link to={{ pathname: '/verify' }} className="dropdown-item">Sign Up</Link> : "Sign Up"}
-                </button>
-              </div>
-              <div className="ml-auto mt-auto mb-auto  large-login valdationtext">
-                <span>Already have an account? <strong style={{cursor:"pointer"}} onClick={this.togglePage} className="text-premium bold">Login!</strong></span>
-              </div>
-            </div>
-          </div>
-        </div>
+          </React.Fragment>
         </Switch>
-
       );
     }
   }
