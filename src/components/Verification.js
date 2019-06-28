5dbc15c800e2d92ce722980bf59b90c622dfec61
omar7altawil <omar.altawil@hotmail.com>
2019-06-28 15:47:19 +0200

    clean

--- src/components/Verification.js
+++ src/components/Verification.js
@@ -7,24 +7,24 @@ export default class Verification extends Component {
         this.state={
             text:null
         }
-        this.verfy = this.verfy.bind(this);
+        this.verify = this.verify.bind(this);
       }
     
 
-    verfy(){
+      verify(){
         const user = firebase.auth().currentUser;
         console.log(user)
         user.sendEmailVerification().then(function() {
         }).catch(function(error) {
             console.log(error)
         });
-        this.setState({text:"email sent to"+user.email})
+        this.setState({text:"email sent to "+user.email})
     }
     render() {
         return (
             <div>
-                <h1>Please verfy your email address</h1>
-                <button onClick={this.verfy}>Send verification</button>
+                <h1>Please verify your email address</h1>
+                <button onClick={this.verify}>Send verification</button>
                 <div>{this.state.text ? <h3> this.state.text</h3> : <div></div>}</div>
             </div>
             
