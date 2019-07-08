04dfebbffa6b79037f87d3c67b8d2bf319cb53c2
omar7altawil <omar.altawil@hotmail.com>
2019-07-08 22:37:37 +0200

    sendMail function

--- backend/controllers/trade.js
+++ backend/controllers/trade.js
@@ -1,6 +1,31 @@
-"use strict";
+"use strict"
 
 const firebase = require('../firebaseConfig')
+const nodemailer = require("nodemailer");
+
+
+async function sendMail(to,subject,text,){
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp-mail.outlook.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "tummarket@hotmail.com", // generated ethereal user
+        pass: "Tumm201(" // generated ethereal password
+      }
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"TumMarket"tummarket@hotmail.com', // sender address
+      to: to, // list of receivers
+      subject:  subject, // Subject line
+      text: text, // plain text body
+    });
+  
+  }
+
 
 const getTradeReq = (req, res) => {
     const id = req.params.id;
