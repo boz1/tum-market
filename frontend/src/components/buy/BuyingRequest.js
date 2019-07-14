47483da55bf5db3454aa3e76adfdf5e6d9c73672
UdayMenon <udaynmenon@gmail.com>
2019-07-14 20:17:03 +0200

    Buyer card and detail styling

--- frontend/src/components/buy/BuyingRequest.js
+++ frontend/src/components/buy/BuyingRequest.js
@@ -17,20 +17,21 @@ export default class BuyingRequest extends Component {
         }
         return (
             <div>
-                <Card style={{ width: '15rem', marginBottom: "10px" }} className={isPremium ? "premium" : ""}>
-                    <Link to={{ pathname: '/buyDetails/' + this.props.buyingRequest.id, state: { buyingRequest: this.props.buyingRequest, user: this.props.user, categories: this.props.categories, subCategories: this.props.subCategories }, func: this.props.reRender }} className="my-3 mx-auto">
+                <Link to={{ pathname: '/buyDetails/' + this.props.buyingRequest.id, state: { buyingRequest: this.props.buyingRequest, user: this.props.user, categories: this.props.categories, subCategories: this.props.subCategories }, func: this.props.reRender }} className="my-3 mx-auto">
+                    <Card style={{ width: '15rem', marginBottom: "10px" }} className={isPremium ? "premium" : ""}>
                         <Card.Body className="p-2">
+                            <Card.Title className="card-title">{this.props.buyingRequest.title}</Card.Title>
                             <hr></hr>
-                            <Card.Title className="text-ad-title">{this.props.buyingRequest.title}</Card.Title>
-                            <Card.Text>
-                                {this.props.buyingRequest.mainCategory.title}
+                            <Card.Text class="card-subtitle text-right">
+                                {this.props.buyingRequest.mainCategory.title + "  -  "}
+                                <span className="bold text-premium">{this.props.buyingRequest.price + " €"}</span>
                             </Card.Text>
-                            <Card.Text className="bold">
-                                <span className="text-premium">{this.props.buyingRequest.price + " €"}</span>
+                            <Card.Text className="bold text-right">
+                                
                             </Card.Text>
                         </Card.Body>
-                    </Link>
-                </Card>
+                    </Card>
+                </Link>
             </div>
         )
     }
