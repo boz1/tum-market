f4b8a15da01898d28d724ce22a9aac5be83902b9
Burak Oz <oz.burak1995@gmail.com>
2019-10-01 12:25:12 +0200

    update

--- backend/controllers/trade.js
+++ backend/controllers/trade.js
@@ -19,7 +19,7 @@ async function sendMail(to, subject, text, ) {
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-        from: '"Tum Market" tummarket@hotmail.com', // sender address
+        from: "Tum Market", 
         to: to, // list of receivers
         subject: subject, // Subject line
         text: text, // plain text body
